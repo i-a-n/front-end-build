40102990fd6f9e0d6d03c1b3efcfddca7a3ab090
Chris W. Burke <cburke@criticalmass.com>
2017-11-21 17:08:44 -0500

    Fixed an error which prevented instances from being bound

--- src/js/lib/component.js
+++ src/js/lib/component.js
@@ -1,23 +1,27 @@
-import domready from 'domready'
-import ElementExists from '../lib/element-exists'
+import domLoaded from 'dom-loaded'
 import attributes from 'data-attributes'
 import $dom from 'dom-helpers'
 import request from 'superagent'
 import PubSub from 'pubsub-js'
-import utility from '../lib/utility'
+import utility from './utility'
 
-class Component {
-    constructor( ...options ) {
-        Object.assign( this, ...options )
-        domready( this.exists.call( this ) )
+class bmwComponent {
+    constructor(node, ...options ) {
+      Object.assign( this, ...options )
+      domLoaded.then(() => {
+        this.element = node
+        this.ready.call( this )
+      })
     }
-    exists() {
-        const initialize = ElementExists( this.el ) ? true : false
-        const element = document.querySelectorAll( this.el )
-        if ( initialize ) this.mount()
+    ready() {
+      if (!this.element) { this.mount(); return }
+      domLoaded.then(() => { this.mount() })
     }
-    debug(logtype, log) {
-      console[logtype](`Debug: ${this.name}\n`, log)
+    debug( logtype, log ) {
+      console[logtype]( `Debug: ${this.name}\n`, log )
+    }
+    attrs() {
+      return !this.el ? false : attributes(this.element)
     }
     bindSubscriptions() {
       if ( this.subscriptions && typeof this.subscriptions == 'object' ){
@@ -28,31 +32,41 @@ class Component {
         }
       }
     }
+    init() {
+      console.warn(`${this.name} did not provide an init function`)
+    }
     bindEvents() {
       let eventSplitter = /\s+/
+      if ( !this.element && this.events ) {
+        console.warn(`${this.name} requires an element to bind events to`)
+        return
+      }
       if ( this.events && typeof this.events == 'object' ){
         for ( let [ev, cb] of Object.entries( this.events ) ){
           let componentEvent = ev.split( eventSplitter )
           let componentCallback = typeof cb === 'function' ? cb : this[cb]
-          let nodes = document.querySelectorAll( this.el )
           let _self = this
-          nodes.forEach(node => {
-            node.addEventListener( componentEvent[0], function(e){
-                if (e.target.matches(componentEvent[1])) {
-                    componentCallback.bind(_self,e).call()
-                }
-            })
+          this.element.addEventListener( componentEvent[0], function(e){
+              if ( e.target.matches(componentEvent[1]) ) {
+                  componentCallback.bind( _self,e ).call()
+              }
           })
         }
       }
     }
     mount() {
-      this.attrs = attributes(document.querySelector( this.el ))
-      this.init()
       this.bindEvents()
       this.bindSubscriptions()
+      this.init()
     }
 }
 
+const Component = function(...props){
+  const options = props[0]
+  const nodes = document.querySelectorAll( options.el )
+  nodes.forEach(node => {
+    new bmwComponent(node, options)
+  })
+}
 
-export { Component, domready, $dom, request, PubSub, utility }
+export { Component, domLoaded, $dom, request, PubSub, utility }
